a68c5fae4a44b92c74999b5c03f4a1cda00c69a6
Arkadi Listov <arkalis@greeny.cs.tlu.ee>
2015-09-16 13:32:06 +0300

    3tundittoo

--- Page/login.php
+++ Page/login.php
@@ -6,34 +6,70 @@
 	$email_error = "";
 	$password_error = "";
 	
+	$name_error = "";
+	
+	$name = "";
+	
 	// kontrollime et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
 		//echo "keegi vajutas nuppu";
+		if(isset($_POST["login"])){ 
 		
-		//kontrollin et e-post ei ole tühi
-		if ( empty($_POST["email"]) ) {
-			$email_error = "See väli on kohustuslik";
-		}
-		
-		//kontrollin, et parool ei ole tühi
-		if ( empty($_POST["password"]) ) {
-			$password_error = "See väli on kohustuslik";
-		} else {
+			echo "vajutas login nuppu";
 			
-			// kui oleme siia jõudnud, siis parool ei ole tühi
-			// kontrollin, et oleks vähemalt 8 sümbolit pikk
-			if(strlen($_POST["password"]) < 8) { 
-			
-				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
-				
+			//kontrollin et e-post ei ole tühi
+			if ( empty($_POST["email"]) ) {
+				$email_error = "See väli on kohustuslik";
 			}
 			
-		}
+			if ( empty($_POST["name"]) ) {
+				$name_error = "See väli on kohustuslik";
+			}
+			
+			//kontrollin, et parool ei ole tühi
+			if ( empty($_POST["password"]) ) {
+				$password_error = "See väli on kohustuslik";
+			} else {
+				
+				// kui oleme siia jõudnud, siis parool ei ole tühi
+				// kontrollin, et oleks vähemalt 8 sümbolit pikk
+				if(strlen($_POST["password"]) < 8) { 
+				
+					$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
+					
+				}	
+			}
 		
+		//keegi vajutas create nuppu
+		}elseif(isset($_POST["create"])){
+			
+			echo "vajutas create nuppu!";
+			
+			//valideerimine create user vormile
+			//kontrollin et e-post ei ole tühi
+			if ( empty($_POST["name"]) ) {
+				$name_error = "See väli on kohustuslik";
+			}else{
+				//kõik on korras
+				// test_input eemaldab pahatahktlikut osad
+				$name = test_input($_POST["name"]);
+			}
+		
+		}
 	}
 	
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+	
+	
+	
 ?>
+
 <?php
 	$page_title = "Sisselogimise leht";
 	$page_file_name = "login.php";
@@ -52,8 +88,8 @@
 	
 	<h2>Create user</h2>
 	<form action="login.php" method="POST" >
-		<input name="First name" placeholder="First name"> <br><br>
-		<input name="Last name" placeholder="Last name"> <br><br>
+		<input name="First name" placeholder="First name">* <?php echo $name_error; ?> <br><br>
+		<input name="Last name" placeholder="Last name">* <?php echo $name_error; ?> <br><br>
 		<input name="email" placeholder="Email"> <?php echo $email_error; ?> <br><br>
 		<input name="email" placeholder="Re-enter Email"> <?php echo $email_error; ?> <br><br>
 				<?php
